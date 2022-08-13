c2b789bd634f0938357d2163414ac213348f4b51
yewon-Noh <yewo2nn16@gmail.com>
2022-08-13 22:57:21 +0900

    #1 - API 응답 형식 작성

---
+++ src/main/java/inhatc/insecure/veganday/common/model/ResponseFmt.java
@@ -0,0 +1,29 @@
+package inhatc.insecure.veganday.common.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResponseFmt<T> {
+    private int statusCode;
+    private String responseMessage;
+    private T data;
+
+    @Builder
+    public ResponseFmt(final int statusCode, final String responseMessage, final  T data) {
+        this.statusCode = statusCode;
+        this.responseMessage = responseMessage;
+        this.data = data;
+    }
+
+    public static<T> ResponseFmt<T> res(final int statusCode, final String responseMessage, final T t) {
+        return ResponseFmt.<T>builder()
+                .data(t)
+                .statusCode(statusCode)
+                .responseMessage(responseMessage)
+                .build();
+    }
+}
--- src/main/java/inhatc/insecure/veganday/community/controller/CommunityController.java
+++ src/main/java/inhatc/insecure/veganday/community/controller/CommunityController.java
@@ -1,9 +1,14 @@
 package inhatc.insecure.veganday.community.controller;
 
+import inhatc.insecure.veganday.common.model.ResponseFmt;
+import inhatc.insecure.veganday.common.model.ResponseMsg;
+import inhatc.insecure.veganday.common.model.StatusCode;
 import inhatc.insecure.veganday.community.model.Community;
 import inhatc.insecure.veganday.community.repository.CommunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +23,9 @@ public class CommunityController {
     CommunityRepository communityRepository;
 
     @GetMapping("")
-    public List<Community> findAll(){
-        return communityRepository.findAll(Sort.by(Sort.Direction.DESC, "writeDt"));
+    public ResponseEntity findAll(){
+//        return communityRepository.findAll(Sort.by(Sort.Direction.DESC, "writeDt"));
+        List<Community> list = communityRepository.findAll(Sort.by(Sort.Direction.DESC, "writeDt"));
+        return new ResponseEntity(ResponseFmt.res(StatusCode.OK, ResponseMsg.READ_BOARDS, list), HttpStatus.OK);
     }
 }
