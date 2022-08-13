0908ab80dc2aa06f897722f045db546068457b71
yewon-Noh <yewo2nn16@gmail.com>
2022-08-14 00:45:18 +0900

    #2 - 디비 설정 변경

--- src/main/java/inhatc/insecure/veganday/community/model/Board.java
+++ src/main/java/inhatc/insecure/veganday/community/model/Board.java
@@ -10,28 +10,21 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "board")
 @Entity
-public class Community {
+public class Board {
     @Id
-    @Column(name = "board_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long bid;
 
     @NotNull
-    @Column(name = "board_ttl")
     private String ttl;
 
     @NotNull
-    @Column(name = "board_cn")
     private String cn;
 
-    @Column(name = "board_user_id")
     private String userId;
 
-    @Column(name = "board_hit_ct")
     private long hit;
 
-    @Column(name = "board_write_dt")
     private String writeDt;
 }
