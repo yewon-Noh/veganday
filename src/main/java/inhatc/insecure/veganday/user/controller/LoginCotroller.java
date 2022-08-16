54ca232dff2cbb4316da132d5a68870ded09a3b8
songhwee1 <songhwee1@naver.com>
2022-08-16 22:57:25 +0900

    server test

--- src/main/java/inhatc/insecure/veganday/user/controller/LoginCotroller.java
+++ src/main/java/inhatc/insecure/veganday/user/controller/LoginCotroller.java
@@ -3,7 +3,8 @@ package inhatc.insecure.veganday.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,35 +24,30 @@ import com.google.api.client.json.gson.GsonFactory;
 
 @RestController
 public class LoginCotroller {
-
-    @GetMapping("/login")
+  
+    @CrossOrigin(origins="*")
+    @PostMapping("/login")
     public ResponseEntity list(@RequestParam(required = true) String tokenId) throws GeneralSecurityException, IOException{
+      System.out.println(tokenId);
       GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
       // Specify the CLIENT_ID of the app that accesses the backend:
       .setAudience(Collections.singletonList("903572796053-1h25l5pim005lmje1htgg9g6t7fluscb.apps.googleusercontent.com"))
       // Or, if multiple clients access the backend:
       //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
       .build();
-  
-      // (Receive idTokenString by HTTPS POST)
       
       GoogleIdToken idToken = verifier.verify(tokenId);
       if (idToken != null) {
         Payload payload = idToken.getPayload();
-      
+
         // Print user identifier
         String userId = payload.getSubject();
         System.out.println("User ID: " + userId);
-      
+
         // Get profile information from payload
         String email = payload.getEmail();
-        boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
         String name = (String) payload.get("name");
-        String pictureUrl = (String) payload.get("picture");
-        String locale = (String) payload.get("locale");
-        String familyName = (String) payload.get("family_name");
-        String givenName = (String) payload.get("given_name");
-      
+            
         // Use or store profile information
         // ...
         return new ResponseEntity(ResponseFmt.res(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS, email), HttpStatus.OK);
